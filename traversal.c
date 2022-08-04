86b8aa42d5169530a46ea96263b41c7af685bcd4
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2022-08-05 00:14:08 +0530

    Array traversal

---
+++ traversal.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int A[5]= {4,3,12,7,9};
+    int i;
+    printf("ARRAY ELEMENTS: \n");
+    for(i=0;i<5;i++)
+    {
+        printf("A[%d]= %d\n",i,A[i]);
+    }
+}
