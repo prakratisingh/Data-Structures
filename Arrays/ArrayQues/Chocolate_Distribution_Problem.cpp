567e17dc7eb0e0d4d0db25b8001d7bfa9c77d15d
Prakrati Singh <500082638@stu.upes.ac.in>
2023-01-31 09:27:19 +0530

    Update Chocolate_Distribution_Problem.cpp

--- Arrays/ArrayQues/Chocolate_Distribution_Problem.cpp
+++ Arrays/ArrayQues/Chocolate_Distribution_Problem.cpp
@@ -4,6 +4,7 @@ class Solution{
     public:
     long long findMinDiff(vector<long long> a, long long n, long long m){
     //code
+    //sorting the vector
     sort(a.begin(),a.end());
     long long res = a[m-1] - a[0];
     for(int i = 1; i+m-1 < n; i++){
