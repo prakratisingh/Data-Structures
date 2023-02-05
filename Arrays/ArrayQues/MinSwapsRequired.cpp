addb0f974bc9a6f2bd22381030c248a2881cf416
Prakrati Singh <500082638@stu.upes.ac.in>
2023-02-05 20:16:51 +0530

    Create MinSwapsRequired.cpp

---
+++ Arrays/ArrayQues/MinSwapsRequired.cpp
@@ -0,0 +1,39 @@
+//Ques link: https://practice.geeksforgeeks.org/problems/minimum-swaps-required-to-bring-all-elements-less-than-or-equal-to-k-together4847/1
+
+class Solution
+{
+public:
+    int minSwap(int arr[], int n, int k) {
+        // Complet the function
+        int c = 0;
+        for(int i=0;i<n;i++)
+        {
+            if (arr[i]<=k)
+            {
+                c++;
+            }
+        }
+        int curr = 0;
+        for(int i=0;i<c;i++)
+        {
+            if(arr[i]<=k)
+            {
+                curr++;
+            }
+        }
+        int ans = c-curr;
+        for(int i=c;i<n;i++)
+        {
+            if(arr[i-c]<=k)
+            {
+                curr--;
+            }
+            if(arr[i]<=k)
+            {
+                curr++;
+            }
+            ans = min(c - curr,ans);
+        }
+        return ans;
+    }
+};
