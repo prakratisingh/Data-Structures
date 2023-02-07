c9bef0b130dbfb9002e44707b4fbb97086e6bac1
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2023-02-07 22:54:03 +0530

    Add files via upload

---
+++ Bitwise_Manipulation/Binary_Representation.cpp
@@ -0,0 +1,26 @@
+//Ques link = https://practice.geeksforgeeks.org/problems/binary-representation5003/1
+
+class Solution
+{
+public:
+    string getBinaryRep(int N)
+    {
+        // Write Your Code here
+        string res;
+        while(N!=0)
+        {
+            res=to_string(N%2)+res;
+            if(N==1)
+            {
+                break;
+            }
+            N=N/2;
+        }
+        string s;
+        for(int i=1;i<=30-res.length();i++)
+        {
+            s+="0";
+        }
+        return s+res;
+    }
+};
