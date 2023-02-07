60e436cb8ac26463bf13dcf6acb236ef7e4bc172
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2023-02-07 22:53:42 +0530

    Add files via upload

---
+++ Bitwise_Manipulation/Basics.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a = 4,b=2;
+
+    //Bitwise AND
+    cout<<"Bitwise AND: "<<(a&b)<<endl;
+
+    //Bitwise OR
+    cout<<"Bitwise OR: "<<(a|b)<<endl;
+
+    //Bitwise XOR
+    cout<<"Bitwise XOR: "<<(a^b)<<endl;
+
+    //Bitwise NOT
+    cout<<"Bitwise NOT: "<<(~a)<<endl;
+
+    //Left shift
+    cout<<"Left shift: "<<(a<<1)<<endl;
+
+    //Right shift
+    cout<<"Right shift: "<<(a>>1)<<endl;
+
+}
