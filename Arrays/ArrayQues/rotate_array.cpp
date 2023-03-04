e5219d8136e157a1bccf893b2a2502b4888b8c82
Prakrati Singh <500082638@stu.upes.ac.in>
2023-03-04 22:22:34 +0530

    Create rotate_array.cpp

---
+++ Arrays/ArrayQues/rotate_array.cpp
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) 
+    {
+        k = k % nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+    }
+};
