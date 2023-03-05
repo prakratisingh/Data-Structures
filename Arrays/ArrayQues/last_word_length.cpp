e7e205607d784a67c26801d6bc504ce7461c2619
Prakrati Singh <500082638@stu.upes.ac.in>
2023-03-05 22:25:04 +0530

    Create last_word_length.cpp

---
+++ Arrays/ArrayQues/last_word_length.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0, tail = s.length() - 1;
+        while (tail >= 0 && s[tail] == ' ') tail--;
+        while (tail >= 0 && s[tail] != ' ') {
+            len++;
+            tail--;
+        }
+        return len;
+    }
+};
