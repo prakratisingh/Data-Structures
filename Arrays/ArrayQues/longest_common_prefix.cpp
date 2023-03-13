8af63e001de977eb24ed09479bc90f7fe9b7091f
Prakrati Singh <500082638@stu.upes.ac.in>
2023-03-13 22:38:47 +0530

    Update longest_common_prefix.cpp

--- Arrays/ArrayQues/longest_common_prefix.cpp
+++ Arrays/ArrayQues/longest_common_prefix.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/longest-common-prefix/?envType=study-plan-v2&envId=top-interview-150
+//ques: https://leetcode.com/problems/longest-common-prefix/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
