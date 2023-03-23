fe83c33a8af2435ed48e8d2e0834727887e5cbd5
Prakrati Singh <500082638@stu.upes.ac.in>
2023-03-23 22:50:24 +0530

    Update jump_game.cpp

--- Arrays/ArrayQues/jump_game.cpp
+++ Arrays/ArrayQues/jump_game.cpp
@@ -1,4 +1,4 @@
-//ques link: https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
+//queslink: https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
