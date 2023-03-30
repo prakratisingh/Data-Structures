6317add380f8e14c9b46a417d5f9114844b9ee98
Prakrati Singh <500082638@stu.upes.ac.in>
2023-03-30 22:43:00 +0530

    Create jump_game.cpp

---
+++ Arrays/ArrayQues/jump_game.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int maxreach = 0;
+        for (int i=0;i<n;i++){
+            if(i>maxreach)
+                return false;
+            maxreach = max(maxreach,nums[i]+i);
+        }
+        return true;
+    }
+};
