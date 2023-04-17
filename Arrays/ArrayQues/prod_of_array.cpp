6db0796364f4eb704689b07428d90ad1ab7fda45
Prakrati Singh <500082638@stu.upes.ac.in>
2023-04-17 23:01:53 +0530

    Update prod_of_array.cpp

---
+++ Arrays/ArrayQues/prod_of_array.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/product-of-array-except-self/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> out(n);
+        out[0] = 1;
+        for(int i=1;i<n;i++)
+        {
+            out[i] = out[i-1]*nums[i-1];
+        }
+        int r = 1;
+        for(int i=n-2;i>=0;--i)
+        {
+            r*=nums[i+1];
+            out[i]*=r;
+        }
+        return out;
+    }
+};
