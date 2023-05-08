364ef8bb259f33d30bebb263448a028da0e16603
Prakrati Singh <500082638@stu.upes.ac.in>
2023-05-08 23:10:31 +0530

    Update insert_delete_get_random.cpp

--- Arrays/ArrayQues/insert_delete_get_random.cpp
+++ Arrays/ArrayQues/insert_delete_get_random.cpp
@@ -8,10 +8,8 @@ private:
     unordered_map<int,int> m;
 public:
     RandomizedSet() 
-    {
-        
+    {   
     }
-    
     bool insert(int val) 
     {
         if(m.find(val)!=m.end())
@@ -45,10 +43,3 @@ public:
     }
 };
 
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
