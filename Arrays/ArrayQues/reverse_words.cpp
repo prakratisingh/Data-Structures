e418a19fb6630218518d3ef1f6d200a7284c7eb1
Prakrati Singh <500082638@stu.upes.ac.in>
2023-05-17 23:15:54 +0530

    Update reverse_words.cpp

--- Arrays/ArrayQues/reverse_words.cpp
+++ Arrays/ArrayQues/reverse_words.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=top-interview-150
+//ques link: https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
