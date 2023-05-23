d5114e2a4769712583bc7a174592b9fada69a94c
Prakrati Singh <500082638@stu.upes.ac.in>
2023-05-23 23:30:42 +0530

    Update 3_sum.cpp

---
+++ Two Pointers/3_sum.cpp
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/3sum/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> output;
+        for(int i=0; i<n-1; i++){
+            int low = i+1, high = n-1;
+            while(low < high){
+                if(nums[i] + nums[low] + nums[high] < 0){
+                    low++;
+                }
+                else if(nums[i] + nums[low] + nums[high] > 0){
+                    high--;
+                }
+                else{
+                    output.push_back({nums[i], nums[low], nums[high]});
+                    int tempIndex1 = low, tempIndex2 = high;
+                    while(low < high && nums[low] == nums[tempIndex1]) low++;
+                    while(low < high && nums[high] == nums[tempIndex2]) high--;
+                }
+            }
+            while(i+1 < n && nums[i] == nums[i+1]) i++;
+        }
+        return output;
+    }    
+};
