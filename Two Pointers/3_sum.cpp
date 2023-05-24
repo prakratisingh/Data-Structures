1906bd95a7241fe2511fb6dd922134b6d669317d
Prakrati Singh <500082638@stu.upes.ac.in>
2023-05-24 23:31:23 +0530

    Update 3_sum.cpp

--- Two Pointers/3_sum.cpp
+++ Two Pointers/3_sum.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/3sum/?envType=study-plan-v2&envId=top-interview-150
+//ques link: https://leetcode.com/problems/3sum/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 #include<vector>
 using namespace std;
