58d945722de1fa433cafc9fbfd3d3dff8ab6af93
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2023-06-07 18:23:13 +0530

    using stl

---
+++ Arrays/ArrayQues/allpermutations.cpp
@@ -0,0 +1,27 @@
+//using stl
+#include<bits/stdc++.h>
+using namespace std;
+void display(int a[],int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        cout<<a[i]<<"  ";
+    }
+    cout<<endl;
+}
+void find_perm(int a[],int n)
+{
+    sort(a,a+n);
+    do{
+        display(a,n);
+    }
+    while
+        (next_permutation(a,a+n));
+}
+int main()
+{
+    int a[] = {10, 20, 30, 40};
+    int n = sizeof(a)/sizeof(a[0]);
+    find_perm(a,n);
+    return 0;
+}
