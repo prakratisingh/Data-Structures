f9978b36364a48d7f377e7bba6edc74f71b03648
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-09 21:01:12 +0530

    Update happy_number.cpp

--- Hashmap/happy_number.cpp
+++ Hashmap/happy_number.cpp
@@ -1,12 +1,14 @@
 //ques link: https://leetcode.com/problems/happy-number/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     int solve(int n) 
     {
         int sum = 0;
-        while(n > 0) {
+        while(n > 0) 
+        {
 			int r = n%10;
             sum += r*r;
             n /= 10;
@@ -19,7 +21,6 @@ public:
         do {
             slow = solve(slow);
             fast = solve(solve(fast));
-            
             if(fast == 1) return 1;
         } 
         while(slow != fast);
