518c6932cff60e208565e56375f403ce1cfa75e5
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-14 19:36:24 +0530

    Create container_with_most_water.cpp

---
+++ Two Pointers/container_with_most_water.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>& height) 
+    {
+        int water_area = 0;
+        int i=0, j=height.size()-1;
+        while(i<j)
+        {
+            int h = min(height[i],height[j]);
+            water_area = max(water_area, (j-i)*h);
+            while(height[i]<=h && i<j) i++;
+            while(height[j]<=h && i<j) j--;
+        }
+        return water_area;
+    }
+};
