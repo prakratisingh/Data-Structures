edb4c323709a01beafea040200e7adfc66bbf5ea
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-17 19:38:47 +0530

    Update is_subsequesnce.cpp

---
+++ Two Pointers/is_subsequesnce.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) 
+    {
+        int j = 0;
+        for (int i = 0; i < t.length() && j < s.length(); i++)
+        if (s[j] == t[i])
+            j++;
+        return(j==s.length());
+    }
+};
