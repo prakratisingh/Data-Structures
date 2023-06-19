76e4bac69a67080b4c9a99090a3bd288bfe377a1
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-19 19:41:10 +0530

    Update is_subsequesnce.cpp

--- Two Pointers/is_subsequesnce.cpp
+++ Two Pointers/is_subsequesnce.cpp
@@ -10,6 +10,6 @@ public:
         for (int i = 0; i < t.length() && j < s.length(); i++)
         if (s[j] == t[i])
             j++;
-        return(j==s.length());
+        return(j == s.length());
     }
 };
