a860b5a395ba37d4904d7c1284bb3cd888511506
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-23 21:04:54 +0530

    Update ransom_note.cpp

---
+++ Hashmap/ransom_note.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/ransom-note/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) 
+    {
+        map<char,int> map;
+        for (int i=0;i<magazine.length();i++)
+        {
+            map[magazine[i]]++;
+        }
+
+        for(int i=0;i<ransomNote.length();i++)
+        {
+            if(map[ransomNote[i]]>0)
+                map[ransomNote[i]]--;
+                else
+                    return false;
+        }
+
+        return true;
+    }
+};
