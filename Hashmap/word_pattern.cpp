93df70d76f9a92893d69ed5568524bbccfdd743f
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-24 21:13:24 +0530

    Update word_pattern.cpp

--- Hashmap/word_pattern.cpp
+++ Hashmap/word_pattern.cpp
@@ -1,14 +1,17 @@
 //ques link: https://leetcode.com/problems/word-pattern/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
-bool wordPattern(string pattern, string str) {
+bool wordPattern(string pattern, string str) 
+{
     map<char, int> p2i;
     map<string, int> w2i;
     istringstream in(str);
     int i = 0, n = pattern.size();
-    for (string word; in >> word; ++i) {
+    for (string word; in >> word; ++i) 
+    {
         if (i == n || p2i[pattern[i]] != w2i[word])
             return false;
         p2i[pattern[i]] = w2i[word] = i + 1;
