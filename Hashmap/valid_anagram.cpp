7c9f43fea1392c7bae154fc3977dc39f32409791
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-24 21:11:17 +0530

    Update valid_anagram.cpp

--- Hashmap/valid_anagram.cpp
+++ Hashmap/valid_anagram.cpp
@@ -1,7 +1,8 @@
 //ques link: https://leetcode.com/problems/valid-anagram/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     bool isAnagram(string s, string t) 
     {
