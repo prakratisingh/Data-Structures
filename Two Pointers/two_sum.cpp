b2d2db762417eb7a6e04c18966acc1b7f62e7a0f
Prakrati Singh <500082638@stu.upes.ac.in>
2023-06-30 19:45:35 +0530

    Update two_sum.cpp

--- Two Pointers/two_sum.cpp
+++ Two Pointers/two_sum.cpp
@@ -5,7 +5,8 @@ using namespace std;
 class Solution 
 {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int>& numbers, int target) 
+    {
         int left = 0;
         int right = numbers.size() - 1;
         vector<int> res;
