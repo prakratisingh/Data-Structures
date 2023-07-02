26c41c1a0c97124531223bbbf5a0614a9accd8fc
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-02 19:51:10 +0530

    Update valid_palindrome.cpp

---
+++ Two Pointers/valid_palindrome.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(string s) 
+    {
+        int n=s.size(), l=0, r=n-1;
+        while(l<r){
+            while(l<r && !isalnum(s[l])){
+                l++;
+            }
+            while(l<r && !isalnum(s[r])){
+                r--;
+            }
+            if(tolower(s[l++]) != tolower(s[r--])){
+                return false;
+            }
+        }
+        return true;
+    }
+};
