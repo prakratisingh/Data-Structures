f950d6eb118bf5a12ec88c1164ff088423942936
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-02 19:51:47 +0530

    Update valid_palindrome.cpp

--- Two Pointers/valid_palindrome.cpp
+++ Two Pointers/valid_palindrome.cpp
@@ -1,7 +1,8 @@
 //ques link: https://leetcode.com/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     bool isPalindrome(string s) 
     {
