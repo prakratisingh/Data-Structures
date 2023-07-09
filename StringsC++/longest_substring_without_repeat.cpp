2562b59a9024cf5a8ba831e8e300ac9a7b31ad77
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-09 20:17:12 +0530

    Update longest_substring_without_repeat.cpp

---
+++ StringsC++/longest_substring_without_repeat.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        vector<int> dict(256,-1);
+        int maxLen = 0, start=-1;
+        for(int i=0;i!=s.length();i++)
+        {
+            if(dict[s[i]]>start)
+                start=dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i-start);
+        }
+        return maxLen;
+    }
+};
