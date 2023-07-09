1d8370c822b0c1a50cd62c31209e309710306380
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-09 20:18:38 +0530

    Update longest_substring_without_repeat.cpp

--- StringsC++/longest_substring_without_repeat.cpp
+++ StringsC++/longest_substring_without_repeat.cpp
@@ -7,7 +7,7 @@ public:
     int lengthOfLongestSubstring(string s) 
     {
         vector<int> dict(256,-1);
-        int maxLen = 0, start=-1;
+        int maxLen=0, start=-1;
         for(int i=0;i!=s.length();i++)
         {
             if(dict[s[i]]>start)
