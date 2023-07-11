3d528c9439a49c592533b4069e26988c8ea565c5
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-11 20:21:53 +0530

    Update max_size_subarray.cpp

--- Arrays/ArrayQues/max_size_subarray.cpp
+++ Arrays/ArrayQues/max_size_subarray.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
+//ques link: https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
