8ddbb16fdd313c9b3e9dd3f1d0134e4a819d1089
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-11 20:22:19 +0530

    Update max_size_subarray.cpp

--- Arrays/ArrayQues/max_size_subarray.cpp
+++ Arrays/ArrayQues/max_size_subarray.cpp
@@ -1,7 +1,8 @@
 //ques link: https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     int minSubArrayLen(int target, vector<int>& nums) 
     {
