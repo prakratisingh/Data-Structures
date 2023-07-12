9550e8038bf170a38c3014dbd3e044159203515b
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-12 20:25:39 +0530

    Update rotate_image.cpp

--- Matrix/rotate_image.cpp
+++ Matrix/rotate_image.cpp
@@ -1,18 +1,21 @@
 //ques link: https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     void rotate(vector<vector<int>>& matrix) 
     {
         int n = matrix.size();
         int m = matrix[0].size();
         for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
+            for(int j=0; j<i; j++)
+            {
                 swap(matrix[i][j], matrix[j][i]);
             }
         }
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
+        {
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
