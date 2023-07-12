7388984cf4c41a79ca515ad551c00d72fb0cc9ca
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-12 20:25:20 +0530

    Update rotate_image.cpp

--- Matrix/rotate_image.cpp
+++ Matrix/rotate_image.cpp
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
+//ques link: https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
 class Solution {
