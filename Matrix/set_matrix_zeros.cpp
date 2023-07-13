4004f81a05339b4e98a4d63d3a9268156636e343
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-13 20:27:32 +0530

    Update set_matrix_zeros.cpp

--- Matrix/set_matrix_zeros.cpp
+++ Matrix/set_matrix_zeros.cpp
@@ -1,7 +1,8 @@
 //ques link: https://leetcode.com/problems/set-matrix-zeroes/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     void setZeroes(vector<vector<int>>& matrix) 
     {
@@ -32,20 +33,27 @@ public:
                 }
             }
         }
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+        for(int i=1; i<n; i++)
+        {
+            for(int j=1; j<m; j++)
+            {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
                     matrix[i][j] = 0;
                 }
             }
         }
-        if(flag1 == true){
-            for(int i=0; i<n; i++){
+        if(flag1 == true)
+        {
+            for(int i=0; i<n; i++)
+            {
                 matrix[i][0] = 0;
             }
         }
-        if(flag2 == true){
-            for(int j=0; j<m; j++){
+        if(flag2 == true)
+        {
+            for(int j=0; j<m; j++)
+            {
                 matrix[0][j] = 0;
             }
         }
