00bfc99f1478365bd5cd0571478a51a5e8a774bd
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-16 20:28:59 +0530

    Update spiral_matrix.cpp

--- Matrix/spiral_matrix.cpp
+++ Matrix/spiral_matrix.cpp
@@ -1,7 +1,8 @@
 //https://leetcode.com/problems/spiral-matrix/?envType=study-plan-v2&envId=top-interview-150
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) 
     {
