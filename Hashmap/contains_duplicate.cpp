b69cafaa61bd11b5ec39131338d1932c3b766732
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-18 20:55:50 +0530

    Update contains_duplicate.cpp

---
+++ Hashmap/contains_duplicate.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/contains-duplicate-ii/?envType=study-plan-v2&envId=top-interview-150
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) 
+    {
+        unordered_map<int, size_t> map;
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+            if (map.count(nums[i]))
+            {
+                if (i - map[nums[i]] <= k)
+                    return true;
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
