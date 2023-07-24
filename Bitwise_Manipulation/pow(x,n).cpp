565f1eeb63871ccae4df3652523b8fc3d96562d3
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-24 22:02:25 +0530

    Update pow(x,n).cpp

--- Bitwise_Manipulation/pow(x,n).cpp
+++ Bitwise_Manipulation/pow(x,n).cpp
@@ -1,1 +1,25 @@
 //ques link: https://leetcode.com/problems/powx-n/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        if(n<0){
+            x = 1/x;
+        }
+        long num = labs(n);
+
+        double pow = 1;
+
+        while(num){
+            if(num&1)
+            {
+                pow*=x;
+            }
+            x*=x;
+            num>>=1;
+        }
+        return pow;
+    }
+};
