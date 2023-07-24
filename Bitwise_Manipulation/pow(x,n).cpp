bda2c0794aecfd694fb7e1d04a13f93aa180e636
Prakrati Singh <500082638@stu.upes.ac.in>
2023-07-24 22:13:28 +0530

    Update pow(x,n).cpp

--- Bitwise_Manipulation/pow(x,n).cpp
+++ Bitwise_Manipulation/pow(x,n).cpp
@@ -5,14 +5,16 @@ class Solution {
 public:
     double myPow(double x, int n) 
     {
-        if(n<0){
+        if(n<0)
+        {
             x = 1/x;
         }
         long num = labs(n);
 
         double pow = 1;
 
-        while(num){
+        while(num)
+        {
             if(num&1)
             {
                 pow*=x;
